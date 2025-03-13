efacfac7fbd27f961af867fecf38031ae0c99012
Eugene Pau Yin Chi <ycpau.eugene@gmail.com>
2025-03-13 16:25:14 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,26 +1,46 @@
-import feedparser
+import pandas as pd
+import yfinance as yf
+from datetime import datetime, timedelta
 
 # Yahoo News RSS URL
-RSS_URL = "https://www.yahoo.com/news/rss"
-OUTPUT_FILE = "headlines.txt"
+TICKER_SYMBOL = "AAPL"
+WINDOW_DAYS = 30
 
-def fetch_yahoo_news():
-    """抓取 Yahoo News RSS 並儲存到檔案"""
-    feed = feedparser.parse(RSS_URL)
+def get_dates():
+    # Get today's date
+    raw_today = datetime.today()
+    today = raw_today.strftime("%Y-%m-%d")
+    
+    # Get the start-of-record day
+    raw_start_date = raw_today - timedelta(days = WINDOW_DAYS)
+    start_date = raw_start_date.strftime("%Y-%m-%d")
 
-    if not feed.entries:
-        print("⚠️ 沒有找到任何新聞標題，請檢查 RSS 來源！")
-        return
+    return start_date, today
 
-    print("✅ 成功抓取 Yahoo News 頭條新聞！\n")
+def parse_from_yfinance():
+    # Get Dates
+    start_date, today = get_dates()
+    
+    # Fetch stock data
+    stock = yf.download(TICKER_SYMBOL, start_date, today)
+    print(stock.isnull().sum())  # Check for missing data
 
-    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-        for i, entry in enumerate(feed.entries[:10], 1):
-            if "title" in entry and "link" in entry:  # 確保標題和連結存在
-                headline = f"{i}. {entry.title}\n{entry.link}\n\n"
-                f.write(headline)
 
-    print("✅ 新聞標題已成功寫入 headlines.txt")
+    # Check if DataFrame is empty, else synthesis stock data
+    if stock.empty:
+        print("⚠️ Error: Stock data fetch failed! DataFrame is empty.")
+            
+    else:
+        print("✅ Stock data fetched successfully!")
 
-if __name__ == "__main__":
-    fetch_yahoo_news()
+
+    # Save as Parquet
+    stock.to_parquet(f"stock_data_{today}.parquet", engine="pyarrow", index=True)
+    
+    # Restore from Parquet
+    stock_restored = pd.read_parquet(f"stock_data_{today}.parquet", engine="pyarrow")
+
+
+print('開始執行...')
+parse_from_yfinance()
+print('執行完成')
